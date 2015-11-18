c433acc34c8154ed369f2cf70a0b7acb261c6f1c
orlandodb <orlando.benea@insa-lyon.fr>
2015-11-18 17:02:17 +0100

    ajouter classes Model

---
+++ src/model/DemandeLivraison.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.Date;
+
+public class DemandeLivraison {
+
+	private int adresseEntrepot;
+
+	public DemandeLivraison(int adresseEntrepot) {
+		super();
+		this.adresseEntrepot = adresseEntrepot;
+	}
+
+	public int getAdresseEntrepot() {
+		return adresseEntrepot;
+	}
+
+	public void setAdresseEntrepot(int adresseEntrepot) {
+		this.adresseEntrepot = adresseEntrepot;
+	}
+	
+	public int ajouterLivraison(Livraison livraison,Date heureDebut,Date heureFin){
+		
+	}
+	
+	public int supprimerLivraison(int idLivraison1, Date heureDebut1,int idLivraison2, Date heureDebut2){
+		
+	}
+	
+	public void echangerLivraison(int idLivraison1, Date heureDebut1,int idLivraison2, Date heureDebut2){
+		
+	}
+}
