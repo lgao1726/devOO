ee2e3754710ed2de6062216169a2a6f47b58acbc
ahazat <ahazat.iliass@gmail.com>
2015-11-23 08:40:36 +0100

    Create ExceptionEtat.java

---
+++ src/controleur/ExceptionEtat.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package controleur;
+
+/**
+ * Gestion des exceptions au niveau de controleur
+ * @author InterCorp
+ *
+ */
+public class ExceptionEtat extends Exception {
+
+	/**
+	 * 
+	 */
+	public ExceptionEtat(String message) 
+	{
+		super(message);
+	}
+
+}
