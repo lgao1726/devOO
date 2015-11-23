2c599bcf753b7a2e11d8551ccbe44597df052ddd
ahazat <ahazat.iliass@gmail.com>
2015-11-23 08:42:00 +0100

    Update Livraison.java

--- src/modele/Livraison.java
+++ src/modele/Livraison.java
@@ -1,54 +1,42 @@
 package modele;
 
+import java.util.Observable;
 
-public class Livraison 
+/**
+ * Classe livraison
+ * @author InterCorp
+ *
+ */
+public class Livraison extends Observable
 {
 	private int id;
 	private Noeud adresse;
 	private int client;
-	private FenetreLivraison fenetre;
 	
-	public Livraison(int id, Noeud adresse, int client, FenetreLivraison fenetre) 
+	public Livraison(int id, Noeud adresse, int client) 
 	{
 		super();
 		this.id = id;
 		this.adresse = adresse;
 		this.client = client;
-		this.fenetre = fenetre;
+	
 	}
 
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public Noeud getAdresse() {
 		return adresse;
-	}
-
-	public void setAdresse(Noeud adresse) {
-		this.adresse = adresse;
 	}
 
 	public int getClient() {
 		return client;
 	}
-
-	public void setClient(int client) {
-		this.client = client;
-	}
-
-	public FenetreLivraison getFenetre() {
-		return fenetre;
-	}
-
-	public void setFenetre(FenetreLivraison fenetre) {
-		this.fenetre = fenetre;
-	}
 	
 	
-	
+	public void accepte(Visiteur v)
+	{
+		v.visite(this);
+	}
 }
