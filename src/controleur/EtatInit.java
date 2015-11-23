9ae62695f2c5c98bfede1dc6f14a77178c0bc1ef
ahazat <ahazat.iliass@gmail.com>
2015-11-23 08:39:06 +0100

    Update EtatInit.java

--- src/controleur/EtatInit.java
+++ src/controleur/EtatInit.java
@@ -12,18 +12,29 @@ import org.xml.sax.SAXException;
 
 import modele.Plan;
 
-public class EtatInit extends EtatDefaut{
-
-	public  void chargerPlan(Plan planDeVille, Fenetre fenetre)
+public class EtatInit extends EtatDefaut
+{
+	/**
+	 * Méthode qui charger un plan
+	 * @param Plan de ville
+	 * @param Fenetre
+	 * @throws ExceptionEtat
+	 */
+	@Override
+	public void chargerPlan(Plan planDeVille, Fenetre fenetre)
 	{
-		try {
+		try 
+		{
 			DeserialiseurXML.traiterPlan(planDeVille);
+			
 			Controleur.setEtatCourant(Controleur.etatPlanCharge);
-		} catch (ParserConfigurationException 
-				| SAXException | IOException 
-				| ExceptionXML | NumberFormatException e) {
-			//fenetre.afficheMessage(e.getMessage());
-			//plan.reset(largeur, hauteur);
+			
+		} 
+		catch (Exception e) 
+		{
+			fenetre.afficheMessageBox("Erreur au niveau de chargement de plan");
+			planDeVille.reset(0);
 		}
 	}
+	
 }
