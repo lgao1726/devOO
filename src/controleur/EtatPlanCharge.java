a32a9a6564f9f7b46c0c218dadae30932f274523
ahazat <ahazat.iliass@gmail.com>
2015-11-23 08:39:38 +0100

    Update EtatPlanCharge.java

--- src/controleur/EtatPlanCharge.java
+++ src/controleur/EtatPlanCharge.java
@@ -1,6 +1,40 @@
 package controleur;
 
-public class EtatPlanCharge extends EtatDefaut{
-	
+import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import vue.Fenetre;
+import xml.DeserialiseurXML;
+import xml.ExceptionXML;
+import modele.DemandeLivraison;
+import modele.Plan;
+
+public class EtatPlanCharge extends EtatDefaut
+{	
+	/**
+	 * Méthode qui charge les demande des livraision et qui passe vers l'état LivraisonCharger
+	 * @param Plan
+	 * @param DemandeLivraison
+	 */
+	@Override
+	public void chargerDemandes(Plan plan, Fenetre fenetre) 
+	{
+		try 
+		{
+			DeserialiseurXML.chargerDemandeLivraison(plan);
+			
+			Controleur.setEtatCourant(Controleur.etatLivraisonCharge);
+			
+		} 
+		catch (Exception e) 
+		{
+			fenetre.afficheMessageBox("Erreur au niveau de chargement du demande des livraisons");
+			plan.setDemandeLivraisons(null);
+		}
+	}
+	
+	
 }
