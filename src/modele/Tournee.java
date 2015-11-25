ee0571e8b55d17d43d43332075086e78b5170b57
orlandodb <orlando.benea@insa-lyon.fr>
2015-11-25 10:46:01 +0100

    calculTournee basics

--- src/modele/Tournee.java
+++ src/modele/Tournee.java
@@ -3,15 +3,31 @@ package modele;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import tsp.GrapheLivraison;
+import tsp.TSP;
+import tsp.TSP1;
 
 public class Tournee {
-	private ArrayList<Itineraire> itineraires;
+	
+	private static final int TEMPSLIMITE = 60000;
+	
+	private ArrayList<Itineraire> itineraires;	
 	
 	//constructeur temporaire pour facilier le dev de la vue
 	public Tournee(Plan plan,DemandeLivraison demande){
 		itineraires = new ArrayList<Itineraire>();
-	}
-	public void calculTournee(Plan plan, ArrayList<Livraison>livraison){
+	} 
+	public void calculTournee(Plan plan, ArrayList<FenetreLivraison> fenetreLivraisons){
+		GrapheLivraison grapheLivraison = new GrapheLivraison(plan, fenetreLivraisons);
+		TSP tsp = new TSP1();
+		tsp.chercheSolution(TEMPSLIMITE, grapheLivraison);
+		Queue<Integer> ordreLivraisons = new LinkedList<Integer>();
+		for(int i=0; i<grapheLivraison.getNbSommets(); i++){
+			ordreLivraisons.add(tsp.getSolution(i));
+		}
 		
 	}
 	
