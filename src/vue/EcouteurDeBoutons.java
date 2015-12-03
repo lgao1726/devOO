7b044b9627fc0a5916439925be7598be005a2ef4
ahazat <ahazat.iliass@gmail.com>
2015-12-04 00:08:40 +0100

    Update EcouteurDeBoutons.java

--- src/vue/EcouteurDeBoutons.java
+++ src/vue/EcouteurDeBoutons.java
@@ -1,37 +1,59 @@
-/**package vue;
+package vue;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import controleur.Controleur;
-import controleur.ListeCommandes;
 
-public class EcouteurDeBoutons implements ActionListener {
-	
+/**
+ * Classe EcouteurDeBoutons qui attend un évenement se génére sur les bouttons 
+ * @author H4101 International Corp
+ *
+ */
+public class EcouteurDeBoutons implements ActionListener 
+{
 	private Controleur controleur;
 
-	public EcouteurDeBoutons(Controleur controleur){
+	/**
+	 * Constructeur d'objet
+	 * @param controleur
+	 */
+	public EcouteurDeBoutons(Controleur controleur)
+	{
 		this.controleur = controleur;
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) { 
+	public void actionPerformed(ActionEvent e) 
+	{ 
 		// Methode appelee par l'ecouteur de boutons a chaque fois qu'un bouton est clique
 		// Envoi au controleur du message correspondant au bouton clique
-		switch (e.getActionCommand()){
-		case Fenetre.CHARGER_PLAN: controleur.chargerPlan(); break;
-		case Fenetre.CHARGER_LIVRAISON: controleur.chargerLivraison(); break;
-		case Fenetre.CALCULER_TOURNEE: controleur.calculerTournee(); break;
-		case Fenetre.SUPPRIMER_LIVRAISON: controleur.supprimerLivraison(); break;
-		case Fenetre.ECHANGER_LIVRAISON: controleur.echangerLivraison();break;
-		case Fenetre.AJOUTER_LIVRAISON: controleur.ajouterLivraison();break;
-		case Fenetre.UNDO: controleur.undo();break;
-		case Fenetre.REDO: controleur.redo();break;
-		case Fenetre.ANNULER: controleur.annuler();break;
-		case Fenetre.VALIDER: controleur.valider();break;
-		case Fenetre.GENERER_FEUILLE: controleur.genererFeuilleDeRoute();break;
-
-		
+		switch (e.getActionCommand())
+		{
+		case Fenetre.CHARGER_PLAN:
+			controleur.chargerPlan();
+		break;
+		case Fenetre.CHARGER_LIVRAISON:
+			controleur.chargerLivraison();
+		break;
+		case Fenetre.CALCULER_TOURNEE:
+			controleur.calculerTournee();
+		break;
+		case Fenetre.UNDO:
+			controleur.undo();
+		break;
+		case Fenetre.REDO:
+			controleur.redo();
+		break;
+		case Fenetre.GENERE:
+			controleur.genererFeuilleDeRoute();
+		break;
+		case Fenetre.EXIT:
+			controleur.quitter();
+		break;
+		case Fenetre.ABOUT:
+			controleur.apropos();
+		break;
 		}
 	}
-}**/
+}
