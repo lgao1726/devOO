2921b5d8d44e252ecea629857c0a0777ef0b0fd3
ahazat <ahazat.iliass@gmail.com>
2015-12-04 02:00:02 +0100

    Update CommandeAjouter.java

--- src/controleur/CommandeAjouter.java
+++ src/controleur/CommandeAjouter.java
@@ -1,46 +1,43 @@
 package controleur;
 
-import modele.Livraison;
 import modele.Plan;
 
 /**
- * Classe commande aouter livriason
+ * Classe commande echanger livraison
  * @author H4101 International Corp
  *
  */
-public class CommandeAjouter implements Commande 
+public class CommandeEchanger implements Commande
 {
 	private Plan plan;
-	private Livraison livraison;
-	private Livraison livraisonPrecedente;
-
+	private int livraison1;
+	private int livraison2;
+	
 	/**
 	 * Contructeur d'objet
 	 * @param plan
-	 * @param livraison
-	 * @param livraisonPrecedente
+	 * @param livraison1
+	 * @param livraison2
 	 */
-	public CommandeAjouter(Plan plan,Livraison livraison, Livraison livraisonPrecedente)
+	public CommandeEchanger(Plan plan,int livraison1,int livraison2)
 	{
 		this.plan = plan;
-		this.livraison = livraison;
-		this.livraisonPrecedente=livraisonPrecedente;
+		this.livraison1 = livraison1;
+		this.livraison2 = livraison2;
+	}
+	
+	@Override
+	public void executer() 
+	{
+		plan.getDemandeLivraisons().echangerLivraisonSepares(livraison1, livraison2);
+		plan.updatePlan();
+	}
 
-	}
-	
 	@Override
-	public void executer()
+	public void undo() 
 	{
-		System.out.println(plan);
-		plan.getDemandeLivraisons().ajouterLivraison(livraison, livraisonPrecedente);
+		plan.getDemandeLivraisons().echangerLivraisonSepares(livraison2, livraison1);
 		plan.updatePlan();
 	}
-	
-	@Override
-	public void undo()
-	{
-		plan.getDemandeLivraisons().supprimerLivraison(livraison.getAdresse().getId());
-		plan.updatePlan();
-	}
-	
+
 }
