f799898592a04a22ea9cfc8421cf10856174d23a
ahazat <ahazat.iliass@gmail.com>
2015-12-04 01:59:03 +0100

    Update Commande.java

--- src/controleur/Commande.java
+++ src/controleur/Commande.java
@@ -1,6 +1,19 @@
 package controleur;
 
-public interface Commande {
+/**
+ * Interface commande pour implémenter les deux fonction undo/redo
+ * @author H4101 International Corp
+ *
+ */
+public interface Commande 
+{
+	/**
+	 * Méthode executer la commande
+	 */
 	void executer();
+	
+	/**
+	 * Méthode annuler la commande
+	 */
 	void undo();
 }
