6e24da45d6b57d051c4a9d8afb448aa7d85136ba
ahazat <ahazat.iliass@gmail.com>
2015-12-04 02:17:26 +0100

    Update EtatDefaut.java

--- src/controleur/EtatDefaut.java
+++ src/controleur/EtatDefaut.java
@@ -10,7 +10,8 @@ import vue.Fenetre;
  * @author H4101
  *
  */
-public class EtatDefaut implements Etat {
+public class EtatDefaut implements Etat 
+{
 
 	@Override
 	public void chargerPlan(Plan planDeVille, Fenetre fenetre)
@@ -22,73 +23,49 @@ public class EtatDefaut implements Etat {
 
 
 	@Override
-	public void calculerTournee(Plan plan, Fenetre fenetre) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void calculerTournee(Plan plan, Fenetre fenetre) 
+	{}
 	
 	@Override
-	public void selectionnerLivraison(Plan plan,Livraison livraison, ListeCommandes listeDeCdes, Fenetre fenetre) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void selectionnerLivraison(Plan plan,Livraison livraison, ListeCommandes listeDeCdes, Fenetre fenetre) 
+	{}
 	
 	@Override
-	public void supprimerLivraison(Plan plan, Fenetre fenetre){
-		// TODO Auto-generated method stub
-		
-	}
+	public void supprimerLivraison(Plan plan, Fenetre fenetre)
+	{}
 
 	@Override
-	public void echangerLivraison(Plan plan, ListeCommandes listeDeCdes, Fenetre fenetre) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void echangerLivraison(Plan plan, ListeCommandes listeDeCdes, Fenetre fenetre) 
+	{}
 	
 	@Override
-	public void undo(ListeCommandes listeDeCdes){
-		
-	}
+	public void undo(ListeCommandes listeDeCdes)
+	{}
 
-	public void redo(ListeCommandes listeDeCdes) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void redo(ListeCommandes listeDeCdes) 
+	{}
 
 	@Override
-	public void selectionnerNoeud(Plan plan, Noeud noeud, Fenetre fenetre) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void selectionnerNoeud(Plan plan, Noeud noeud, Fenetre fenetre) 
+	{}
 
 	@Override
-	public void ajouterLivraison(Plan plan, Fenetre fenetre) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void ajouterLivraison(Plan plan, Fenetre fenetre) 
+	{}
 
 	@Override
-	public void annuler(Fenetre fenetre) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void annuler(Fenetre fenetre) 
+	{}
 
 	@Override
-	public void valider(Fenetre fenetre) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void valider(Fenetre fenetre) 
+	{}
 	
 	@Override
-	public void deselectionner(Fenetre fenetre) {
-		
-	}
+	public void deselectionner(Fenetre fenetre) 
+	{}
 
 	@Override
-	public void genererFeuilleDeRoute(Plan plan) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-
+	public void genererFeuilleDeRoute(Plan plan) 
+	{}
 }
